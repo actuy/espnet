470ba378ac48c764f7a2bfa0095834206325fcb7
actuy <841223401@qq.com>
2020-03-25 01:54:45 +0800

    add to sys path

--- espnet/bin/lm_train.py
+++ espnet/bin/lm_train.py
@@ -17,14 +17,15 @@ import sys
 import configargparse
 import numpy as np
 
-from espnet.nets.lm_interface import dynamic_import_lm
-from espnet.optimizer.factory import dynamic_import_optimizer
-from espnet.scheduler.scheduler import dynamic_import_scheduler
-
 if "/home/espnet" in sys.path:
     sys.path.remove("/home/espnet")
 ESPNET_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir))
 sys.path.insert(0, ESPNET_ROOT)
+
+from espnet.nets.lm_interface import dynamic_import_lm
+from espnet.optimizer.factory import dynamic_import_optimizer
+from espnet.scheduler.scheduler import dynamic_import_scheduler
+
 
 
 # NOTE: you need this func to generate our sphinx doc
