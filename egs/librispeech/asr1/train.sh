0ab55dd398c3935d933df8c7eeeb2fa541fdec5b
actuy <841223401@qq.com>
2020-03-26 10:09:50 +0800

    add pure train for libri

--- egs/librispeech/asr1/train.sh
+++ egs/librispeech/asr1/train.sh
@@ -4,8 +4,6 @@
 
 # general configuration
 backend=pytorch
-stage=3       # start from -1 if you need to start from data download
-stop_stage=100
 ngpu=4         # number of gpus ("0" uses cpu, otherwise use gpu)
 nj=32
 debugmode=1
