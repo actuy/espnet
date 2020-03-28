cc72f644b9865bdbe692f76722dbbdbbef2dd20d
actuy <841223401@qq.com>
2020-03-28 17:37:09 +0800

    change dir

--- egs/librispeech/asr1/train_spec.sh
+++ egs/librispeech/asr1/train_spec.sh
@@ -87,6 +87,7 @@ mkdir -p ${expdir}
 dict=${dataprefix}/data/lang_char/${train_set}_${bpemode}${nbpe}_units.txt
 feat_tr_dir=${dataprefix}/${dumpdir}/${train_set}/delta${do_delta}; mkdir -p ${feat_tr_dir}
 feat_dt_dir=${dataprefix}/${dumpdir}/${train_dev}/delta${do_delta}; mkdir -p ${feat_dt_dir}
+resume=${expdir}/results/snapshot.ep.2
 
 ${cuda_cmd} --gpu ${ngpu} ${expdir}/train.log \
         asr_train.py \
